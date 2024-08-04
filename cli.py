55f66173f0a7e429179d64e48ec0c3d6bd590d9f
Hugo Campos <85531201+HC-85@users.noreply.github.com>
2024-08-04 20:50:51 +0000

    added cli

--- cli.py
+++ cli.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
 # When you activate a virtual environment, the PATH is modified to include the virtual environment's bin directory at the front.
+import click
+from mlib.mchange import change
+
+
+@click.command()
+@click.option("--amount", prompt="Amount: ", help="Change calculation")
+def make_change(amount):
+    "Gives change in coins"
+    result = change(float(amount))
+    message = click.style(f"Change for {amount}: ", fg="red")
+    click.echo(message)
+    for coin_change in result:
+        for num, coin in coin_change.items():
+            message = click.style(f"{coin}: {num}", fg="green")
+            click.echo(message)
+
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    make_change()
